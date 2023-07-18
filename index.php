05658c97ea8be5835bea1c4942448e5d15ee882f
Mohsan <mohsin@techchaps.com>
2023-07-18 14:23:47 +0500

    Functions and get_template_part usage

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
+<?php error_reporting(0); ?>
 <?php
 get_header();
-
+pageBanner(array(
+    'title' => 'Welocome To Blog',
+    'subtitle' => 'Latest News'
+));
 ?>
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welocome To Blog</h1>
-        <div class="page-banner__intro">
-          <p>Latest News</p>
-        </div>
-      </div>
-    </div>
+
 
     <div class="container container--narrow page-section">
         <?php 
